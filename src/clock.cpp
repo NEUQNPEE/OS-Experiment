4c666b755c54d80678e86e453d378f31b0ebe99a
Salieri <101504501+ArmouredEvil@users.noreply.github.com>
2023-12-21 20:16:49 +0800

    修改CLOCK算法，加入多个接口，实现细节变动较大

--- src/clock.cpp
+++ src/clock.cpp
@@ -1,96 +1,127 @@
 #include <iostream>
 #include <algorithm>
 #include "memory.h"
+#include "memoryBlock.h"
 
-// 模拟的给文件分配的8个内存块，详见Memory.h。具体实现需要商讨后确定
-Memory mem[8];
+using namespace std;
 
-// 模拟的放入内存的文件分块后的页序列。具体实现需要商讨后确定
-int page[1024];
+// 用于装入文件的八个内存块的块号
+int tempBlockNumber[8] = {0};
 
-// 暂定的文件页接口。具体实现需商讨后确定
-void initialPage()
+// 决定由哪八个内存块装入文件
+void initialTempBlockNumber()
 {
-    for (int i = 0; i < 1024; i++)
+    // 外遍历：装文件的内存块。内遍历：搜索未装页的内存块
+    for (int i = 0; i < 8; i++)
     {
-        page[i] = i % 1024;
+        for (int j = 0; j <= 64; j++)
+        {
+            if (j == 64)
+            {
+                cout << "警告!已无空闲内存块!内存溢出!";
+                exit(0);
+            }
+            if (block[j].pageNumber < 0)
+            {
+                tempBlockNumber[i] = j;
+                break;
+            }
+        }
     }
 }
 
-// 暂定的内存接口。具体实现需商讨后确定
-void initialMemory()
+// 将文件的内容填充到内存中。声明见memory.h。具体实现需商讨后完成
+void fillMemory(int begin, int size)
 {
-    // fill(mem, mem + 8, 0);
 }
 
-void CLOCK()
+// 全局置换CLOCK算法。雏形已有，实现细节还需商讨后完善
+void CLOCK(int page)
 {
+    // 装入的内存块的块号
+    int flag = 0;
+
+    // 查找页是否已在内存里
+    bool flag1 = false;
+    for (int j = 0; j < 8; j++)
+    {
+        if (block[tempBlockNumber[j]].pageNumber == page)
+        {
+            block[tempBlockNumber[j]].status = 1;
+            flag = j;
+            flag1 = true;
+            break;
+        }
+    }
+    if (flag1 == true)
+    {
+        fillMemory(tempBlockNumber[flag], 40);
+        return;
+    }
+
+    // 查找是否有没存页的内存块
+    bool flag2 = false;
+    for (int j = 0; j < 8; j++)
+    {
+        if (block[tempBlockNumber[j]].pageNumber == -1)
+        {
+            block[tempBlockNumber[j]].pageNumber = page;
+            block[tempBlockNumber[j]].status = 1;
+            flag = j;
+            flag2 = true;
+            break;
+        }
+    }
+    if (flag2 == true)
+    {
+        fillMemory(tempBlockNumber[flag], 40);
+        return;
+    }
+
+    // 查找是否有访存位为0的内存块
+    bool flag3 = false;
+    for (int j = 0; j < 8; j++)
+    {
+        if (block[tempBlockNumber[j]].status == 0)
+        {
+            block[tempBlockNumber[j]].pageNumber = page;
+            block[tempBlockNumber[j]].status = 1;
+            flag = j;
+            flag3 = true;
+            break;
+        }
+    }
+    if (flag3 == true)
+    {
+        fillMemory(tempBlockNumber[flag], 40);
+        return;
+    }
+
+    // 页面置换
+    for (int j = 0; j < 8; j++)
+    {
+        block[tempBlockNumber[j]].status = 0;
+    }
+    block[tempBlockNumber[0]].pageNumber = page;
+    // 文件内容填充到内存中
+    fillMemory(tempBlockNumber[0], 40);
+}
+
+// 暂定的文件读入到内存的接口，声明见memoryBlock.h。具体实现需商讨后确定
+void readFile()
+{
+    // 假设的文件分块后的页序列，方便演示
+    int page[1024] = {0};
     for (int i = 0; i < 1024; i++)
     {
-        // 查找页是否已在内存里
-        bool flag1 = false;
-        for (int j = 0; j < 8; j++)
-        {
-            if (mem[j].number == page[i])
-            {
-                mem[j].status = 1;
-                flag1 = true;
-                break;
-            }
-        }
-        if (flag1 == true)
-        {
-            continue;
-        }
-
-        // 查找是否有没存页的内存块
-        bool flag2 = false;
-        for (int j = 0; j < 8; j++)
-        {
-            if (mem[j].number == -1)
-            {
-                mem[j].number = page[i];
-                mem[j].status = 1;
-                flag2 = true;
-                break;
-            }
-        }
-        if (flag2 == true)
-        {
-            continue;
-        }
-
-        // 查找是否有访存位为0的内存块
-        bool flag3 = false;
-        for (int j = 0; j < 8; j++)
-        {
-            if (mem[j].status == 0)
-            {
-                mem[j].number = page[i];
-                mem[j].status = 1;
-                flag3 = true;
-                break;
-            }
-        }
-        if (flag3 == true)
-        {
-            continue;
-        }
-
-        // 页面置换
-        for (int j = 0; j < 8; j++)
-        {
-            mem[j].status = 0;
-        }
-        mem[0].number = page[i];
+        CLOCK(page[i]);
     }
 }
 
 /*
 int main()
 {
-    initialPage();
-    CLOCK();
+    readFile();
     return 0;
 }
 */
