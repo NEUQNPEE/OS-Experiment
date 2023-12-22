02a5b5252bb86f28cce8bd30609236511b34712c
ArmouredEvil <1928774641@qq.com>
2023-12-22 20:50:37 +0800

    内存系统更新

--- src/clock.cpp
+++ src/clock.cpp
@@ -4,6 +4,9 @@
 #include "memoryBlock.h"
 
 using namespace std;
+/*
+* 文件管理系统才知道一个文件使用那八个内存块
+*/
 
 // 用于装入文件的八个内存块的块号
 int tempBlockNumber[8] = {0};
@@ -107,15 +110,66 @@ void CLOCK(int page)
     fillMemory(block[tempBlockNumber[0]].begin, 40);
 }
 
-// 暂定的文件读入到内存的接口，声明见memoryBlock.h。具体实现需商讨后确定
-void readFile()
+
+// 这个是文件管理系统调用的函数，把用户修改过的新的文件数据传回内存
+void WriteFile(int file_id,std::string file_corrent)
 {
-    // 假设的文件分块后的页序列，方便演示
-    int page[1024] = {0};
-    for (int i = 0; i < 1024; i++)
-    {
-        CLOCK(page[i]);
-    }
+
+}
+
+// 文件管理系统已经知道文件所使用的内存块，调用这个
+// 文件管理系统从内存读出某个内存块，传入一个块号，返回一个内存块的内容
+char* ReadMemoryBolcks(int* block_ids)
+{
+
+}
+
+char* ReadMemoryBolck(int block_id)
+{
+
+}
+
+// 文件管理系统不知道文件所使用的内存块，说明内存肯定没有这个文件，调用这个
+// 从磁盘读入文件
+int* readDiskFile(int file_id)
+{
+    // 调用磁盘管理系统的函数，传入file_id
+    // 让磁盘把文件找到并返回文件的起始盘块号
+
+    // 然后调用读取FAT表的函数，根据FAT表确定需要读入的所有磁盘块
+
+    // 调用ReadDiskBlock
+
+    // 返回装入的内存块块号，以数组形式返回
+}
+
+// 读入磁盘块
+void ReadDiskBlock(int block_id)
+{
+    // 调用磁盘系统提供的函数，传入block_id，得到一个块
+
+    // 选择放进那个内存块
+}
+
+// 文件管理系统要求保存某个文件
+void SaveFile(int* block_ids,char* file)
+{
+    // 这传入的是文件所用到的内存块以及整个文件序列化后的char数组
+    // 按照内存块大小吧file切分，依次调用WriteMemoryBolcks把内容写进内存块
+    // 再调用WriteDiskBlock把这些块存入磁盘
+}
+
+// 文件管理系统管往哪个内存块写数据，内存管理系统不管这个
+void WriteMemoryBolcks(int block_id,char* data)
+{
+
+}
+
+// 文件管理系统调用该函数，让内存管理系统把某个文件的内存块存磁盘
+// 文件管理系统知道要存哪八个块
+void WriteDiskBlock(int* block_ids)
+{
+    // 这里调用磁盘系统的函数，把block_ids这几个内存块写回磁盘块
 }
 
 /*
