502ffc24feb0764e94849931b8196818a03f75f7
Salieri <101504501+ArmouredEvil@users.noreply.github.com>
2023-12-24 21:36:09 +0800

    修改CLOCK算法和内存管理。完善了读写文件的接口

--- src/clock.cpp
+++ src/clock.cpp
@@ -2,19 +2,24 @@
 #include <algorithm>
 #include "memory.h"
 #include "memoryBlock.h"
+#include "disk.h"
 
 using namespace std;
 /*
-* 文件管理系统才知道一个文件使用那八个内存块
-*/
+ * 文件管理系统才知道一个文件使用那八个内存块
+ */
 
 // 用于装入文件的八个内存块的块号
-int tempBlockNumber[8] = {0};
+int block_ids[8];
+// 正在装入内存的文件分块后的各文件页。页最多为1024（40KB/40B）
+string page_content[1024];
+//初始化DiskManager类。如果main函数已初始化，此条需注释
+DiskManager disk;
 
 // 决定由哪八个内存块装入文件
-void initialTempBlockNumber()
+void initialBlock_ids()
 {
-    // 外遍历：装文件的内存块。内遍历：搜索未装页的内存块
+    // 外遍历：确定装文件的内存块。内遍历：搜索未装页的内存块
     for (int i = 0; i < 8; i++)
     {
         for (int j = 0; j <= 64; j++)
@@ -24,22 +29,49 @@ void initialTempBlockNumber()
                 cout << "警告!已无空闲内存块!内存溢出!";
                 exit(0);
             }
-            if (block[j].pageNumber < 0)
+            if (memory_block[j].page_id < 0)
             {
-                tempBlockNumber[i] = j;
+                block_ids[i] = memory_block[j].block_id;
                 break;
             }
         }
     }
 }
 
-// 将文件的内容填充到内存中。声明见memory.h。具体实现需商讨后完成
-void fillMemory(int begin, int size)
+
+// 释放装文件的八个内存块
+void clearBlock_ids()
 {
+    for (int i = 0; i < 8; i++)
+    {
+        memory_block[block_ids[i]].page_id = -1;
+        memory_block[block_ids[i]].status = 0;
+        memory_block[block_ids[i]].file_all_in_memory = true;
+    }
+    //顺带把暂存页内容的page_content清空
+    fill(page_content, page_content + 1024, "");
+}
+
+// 将文件页的内容填充到内存中。
+void fillMemory(int page_id, int block_id)
+{
+    int page_size = page_content[page_id].size();
+    int i = 0;
+    int j = memory_block[block_id].begin;
+    for (; i < page_size; i++, j++)
+    {
+        memory[j] = page_content[page_id][i];
+    }
+}
+
+//调用disk.cpp的函数，将修改后的文件内容传进磁盘
+void SaveDiskFile(DiskManager &disk, string block_content, int block_id)
+{
+    disk.store_content_in_exchange_area(block_content, block_id);
 }
 
 // 全局置换CLOCK算法。雏形已有，实现细节还需商讨后完善
-void CLOCK(int page)
+int CLOCK(int page)
 {
     // 装入的内存块的块号
     int flag = 0;
@@ -48,9 +80,9 @@ void CLOCK(int page)
     bool flag1 = false;
     for (int j = 0; j < 8; j++)
     {
-        if (block[tempBlockNumber[j]].pageNumber == page)
+        if (memory_block[block_ids[j]].page_id == page)
         {
-            block[tempBlockNumber[j]].status = 1;
+            memory_block[block_ids[j]].status = 1;
             flag = j;
             flag1 = true;
             break;
@@ -58,18 +90,17 @@ void CLOCK(int page)
     }
     if (flag1 == true)
     {
-        fillMemory(block[tempBlockNumber[flag]].begin, 40);
-        return;
+        return flag;
     }
 
     // 查找是否有没存页的内存块
     bool flag2 = false;
     for (int j = 0; j < 8; j++)
     {
-        if (block[tempBlockNumber[j]].pageNumber == -1)
+        if (memory_block[block_ids[j]].page_id == -1)
         {
-            block[tempBlockNumber[j]].pageNumber = page;
-            block[tempBlockNumber[j]].status = 1;
+            memory_block[block_ids[j]].page_id = page;
+            memory_block[block_ids[j]].status = 1;
             flag = j;
             flag2 = true;
             break;
@@ -77,18 +108,17 @@ void CLOCK(int page)
     }
     if (flag2 == true)
     {
-        fillMemory(block[tempBlockNumber[flag]].begin, 40);
-        return;
+        return flag;
     }
 
     // 查找是否有访存位为0的内存块
     bool flag3 = false;
     for (int j = 0; j < 8; j++)
     {
-        if (block[tempBlockNumber[j]].status == 0)
+        if (memory_block[block_ids[j]].status == 0)
         {
-            block[tempBlockNumber[j]].pageNumber = page;
-            block[tempBlockNumber[j]].status = 1;
+            memory_block[block_ids[j]].page_id = page;
+            memory_block[block_ids[j]].status = 1;
             flag = j;
             flag3 = true;
             break;
@@ -96,38 +126,109 @@ void CLOCK(int page)
     }
     if (flag3 == true)
     {
-        fillMemory(block[tempBlockNumber[flag]].begin, 40);
-        return;
+        return flag;
     }
 
     // 页面置换
     for (int j = 0; j < 8; j++)
     {
-        block[tempBlockNumber[j]].status = 0;
+        memory_block[block_ids[j]].status = 0;
+        memory_block[block_ids[j]].file_all_in_memory = false; // 发生了页面置换，则文件的所有页不可能同时在内存里
     }
-    block[tempBlockNumber[0]].pageNumber = page;
-    // 文件内容填充到内存中
-    fillMemory(block[tempBlockNumber[0]].begin, 40);
+    memory_block[block_ids[0]].page_id = page;
+    return 0;
 }
 
 
 // 这个是文件管理系统调用的函数，把用户修改过的新的文件数据传回内存
-void WriteFile(int file_id,std::string file_corrent)
+// 即：用户写文件
+void WriteFile(int file_id, std::string file_content)
+{
+    // 搜索由哪八个内存块负责装文件
+    initialBlock_ids();
+
+    // 文件分页并装入内存
+    int file_size = file_content.size(); // 文件长度
+    int page_count = file_size / 40;     // 文件分块后的页数
+    int write_block_id = -1;             // 写入的内存块的块号
+    for (int i = 0; i < page_count; i++)
+    {
+        page_content[i] = file_content.substr(i * 40, 40);
+        write_block_id = CLOCK(i);
+        fillMemory(i, write_block_id);
+        SaveDiskFile(disk, page_content[i], write_block_id);    //disk为声明的DiskManager对象
+    }
+
+    // 查找是否有不足40B的尾巴
+    int remainder = file_size % 40;
+    if (remainder != 0)
+    {
+        page_content[page_count] = file_content.substr(page_count * 40, remainder);
+        write_block_id = CLOCK(page_count);
+        fillMemory(page_count, write_block_id);
+        SaveDiskFile(disk, page_content[page_count], write_block_id);    //disk为声明的DiskManager对象
+    }
+
+    //释放内存
+    clearBlock_ids();
+}
+
+//从磁盘读取文件内容
+//没在disk.cpp找到对应接口？？？暂时空置
+char *ReadDiskFile(int file_id)
 {
 
 }
 
-// 文件管理系统已经知道文件所使用的内存块，调用这个
-// 文件管理系统从内存读出某个内存块，传入一个块号，返回一个内存块的内容
-char* ReadMemoryBolcks(int* block_ids)
+//读取内存块中的函数
+char *ReadMemoryBlock(int memory_block_id, int size)
 {
-
+    char *content;
+    int i = memory_block[memory_block_id].begin;
+    for (int j = 0; j < size; j++)
+    {
+        content[j] = memory[i + j];
+    }
+    return content;
 }
 
-char* ReadMemoryBolck(int block_id)
+// 文件管理系统调用，读文件。因为不知道文件管理系统如何接收，所以暂时定为char数组
+char *ReadFile(int file_id)
 {
+    // 搜索由哪八个内存块负责装文件
+    initialBlock_ids();
+    // 从磁盘读出文件内容
+    string file_content = ReadDiskFile(file_id);
 
+    // 其实接下来直接return file_content即可。为了符合指导书的要求，这里仍先装内存，再从内存读出
+    char *ans;
+    // 文件分页并装入内存
+    int file_size = file_content.size(); // 文件长度
+    int page_count = file_size / 40;     // 文件分块后的页数
+    int write_block_id = -1;             // 写入的内存块的块号
+    for (int i = 0; i < page_count; i++)
+    {
+        page_content[i] = file_content.substr(i * 40, 40);
+        write_block_id = CLOCK(i);
+        fillMemory(i, write_block_id);
+        char *temp = ans + i * 40;
+        temp = ReadMemoryBlock(write_block_id, 40);
+    }
+    // 查找是否有不足40B的尾巴
+    int remainder = file_size % 40;
+    if (remainder != 0)
+    {
+        page_content[page_count] = file_content.substr(page_count * 40, remainder);
+        write_block_id = CLOCK(page_count);
+        fillMemory(page_count, write_block_id);
+        char *temp = ans + page_count * 40;
+        temp = ReadMemoryBlock(write_block_id, remainder);
+    }
+    return ans;
 }
+
+/*
+//以下为22日晚讨论出的框架。因思路有所改变，暂时搁置不用
 
 // 文件管理系统不知道文件所使用的内存块，说明内存肯定没有这个文件，调用这个
 // 从磁盘读入文件
@@ -170,12 +271,5 @@ void WriteMemoryBolcks(int block_id,char* data)
 void WriteDiskBlock(int* block_ids)
 {
     // 这里调用磁盘系统的函数，把block_ids这几个内存块写回磁盘块
-}
-
-/*
-int main()
-{
-    readFile();
-    return 0;
 }
 */
