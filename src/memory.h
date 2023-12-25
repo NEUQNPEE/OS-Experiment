b14e6bf6b1a6b2f5c290ed0277784c26974f65da
Salieri <101504501+ArmouredEvil@users.noreply.github.com>
2023-12-25 21:39:10 +0800

    CLOCK算法更新。完善接口，在memory.h注明文件系统调用接口

--- src/memory.h
+++ src/memory.h
@@ -4,3 +4,24 @@ using namespace std;
 
 // 模拟内存
 char memory[2560];
+//内存管理系统存储的fat表
+vector<int> fat_list;
+//文件信息
+char* file_info;
+//目录信息
+char* dir_info;
+
+//初始化内存及相关操作
+void initialMemory();
+
+// 返回文件信息
+char *getFileInfo();
+
+// 返回目录信息
+char *getDirectoryInfo();
+
+// 用户写文件
+void WriteFile(string file_content, char *file_info, char *dir_info);
+
+// 用户读文件
+char *ReadFile();
