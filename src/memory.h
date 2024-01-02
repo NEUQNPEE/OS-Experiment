062523c7838fe736f82404c2b0ae3e89f5779bae
Salieri <101504501+ArmouredEvil@users.noreply.github.com>
2024-01-02 21:36:20 +0800

    内存系统修改，添加“获取内存块调度过程”的接口
    添加“获取内存块调度过程”的接口

--- src/memory.h
+++ src/memory.h
@@ -16,16 +16,16 @@ unordered_map<string, int> file_id_block_map;
 void initialMemory();
 
 // 写入目录信息
-void writeDirectoryInfo(char *info);
+void WriteDirectoryInfo(string file_id, char *info);
 
-// 返回目录信息
-char *getDirectoryInfo();
+// 返回文件的目录信息
+char *ReadDirectoryInfo(string file_id);
 
-// 用户写文件。修改文件和保存文件均调用这个接口
+// 用户写文件内容。修改文件和保存文件均调用这个接口
 void WriteFile(string file_id, string file_content, char *dir_info);
 
-// 用户读文件。此接口会把存在于FAT表中的所有文件读取
+// 用户读文件内容。此接口会把存在于FAT表中的所有文件读取
 string ReadFile(string file_id);
 
-// 用户删除文件。请传入删除文件的盘块号
+// 用户删除文件。请传入删除文件的ID
 void DeleteFile(string file_id);
