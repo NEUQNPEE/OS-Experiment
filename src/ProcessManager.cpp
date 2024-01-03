d57890092ecff1dfbf940a8b355fe1f0b69a5a0d
JlU-YOU <102222347+JlU-YOU@users.noreply.github.com>
2024-01-03 13:06:12 +0800

    进程管理

--- src/ProcessManager.cpp
+++ src/ProcessManager.cpp
@@ -5,6 +5,8 @@
 #include <queue>
 #include <string>
 #include <fcntl.h>
+#include "tree_directory.h"
+
 using namespace std;
 
 /**
@@ -36,13 +38,43 @@ enum class ProcessType {
 };
 
 /**
+ * 操作命令枚举
+ */
+enum class OperationCommand {
+    // 创建文件夹
+    CREATE_FOLDER = 1,
+    // 删除文件夹
+    DELETE_FOLDER = 2,
+    // 创建文件
+    CREATE_FILE = 3,
+    // 删除文件
+    DELETE_FILE = 4,
+    // 进行读写（需要进行内存分配）
+    CREATE_READ_WRITE = 5,
+    // 重命名文件夹
+    RENAME_FOLDER = 6,
+    // 重命名文件
+    RENAME_FILE = 7,
+    // 退出
+    EXIT = 0
+};
+/**
+ * 文件信息结构体
+ */
+struct FileInfo {
+    File *file;
+    Folder *folder;
+    std::string* fileName;
+    std::string* data;
+};
+/**
  * 命名管道结构体
  */
 struct NamedPipe {
     std::string pipeName;
     int fileDescriptor;
 
-    NamedPipe(const std::string& name) : pipeName(name) {
+    NamedPipe(const std::string &name) : pipeName(name) {
         // 创建或打开文件作为命名管道
         fileDescriptor = open(pipeName.c_str(), O_RDWR | O_CREAT, 0666);
     }
@@ -55,7 +87,7 @@ struct NamedPipe {
     }
 
     // 写数据到命名管道
-    void writeData(const std::string& data) {
+    void writeData(const std::string &data) {
         write(fileDescriptor, data.c_str(), data.size());
     }
 
@@ -83,8 +115,8 @@ public:
     ProcessState state;          // 进程状态
     ProcessType type;            // 进程类型
     vector<int> allocatedMemory; // 内存块地址
-    string directory;            //文件存储信息
-    string fileName;
+    FileInfo fileInfo;           // 文件信息
+    OperationCommand command;    // 操作命令
 
     Process(string &name, int pid, int priority, ProcessState state, ProcessType type) {
         this->name = name;
@@ -210,15 +242,15 @@ private:
 
 public:
     static InitProcess create(string name, int pid, int priority, ProcessType processType) {
-        auto* initProcess=new InitProcess(name, pid, priority, processType);
+        auto *initProcess = new InitProcess(name, pid, priority, processType);
         // 将该进程放入进程列表
         processManager.processList.push_back(initProcess);
         return *initProcess;
     }
 
     void execute() override {
-        vector<int> initInfo = DirectoryManager::init();
-        allocatedMemory = DirectoryManager::read_data_to_memory(initInfo);
+        Folder *folder=init();
+        //todo 在这里向QT发送folder指针
     }
 
     void destroy() override {
@@ -241,37 +273,39 @@ public:
             : Process(name, pid, priority, state, type) {}
 
     static void
-    create(string name, int pid, int priority, const std::string &directory, const std::string &fileName) {
-        auto* dataGenerationProcess=new DataGenerationProcess(name, pid, priority, ProcessState::READY,
-                                                    ProcessType::DATA_GENERATION_PROCESS);
-        dataGenerationProcess->fileName = fileName;
-        dataGenerationProcess->directory = directory;
+    create(string name, int pid, int priority,FileInfo* fileInfo,OperationCommand command) {
+        auto *dataGenerationProcess = new DataGenerationProcess(name, pid, priority, ProcessState::READY,
+                                                                ProcessType::DATA_GENERATION_PROCESS);
+        dataGenerationProcess->fileInfo.fileName=fileInfo->fileName;
+        dataGenerationProcess->fileInfo.folder=fileInfo->folder;
+        dataGenerationProcess->command = command;
         // 将该进程放入进程列表,就绪队列
         processManager.processList.push_back(dataGenerationProcess);
         processManager.readyQueue.push(dataGenerationProcess);
     }
 
     void execute() override {
-        //todo 从外界获取dataSize和dataInfo,这里假设直接给出
-        int dataSize = 8;
-        string dataInfo;
-        // 申请所需大小的外存块
-        int allocatedBlocks = DirectoryManager::allocateBlocks(dataSize);
-
-        if (allocatedBlocks < dataSize) {
-            // 盘块不够，给出提示
-            std::cout << "Error: Not enough free disk blocks for data generation." << std::endl;
-            return;
+        judge_is_repeat(fileInfo.folder, *fileInfo.fileName);
+        switch (command) {
+            case OperationCommand::CREATE_FOLDER: {
+                add_folder(fileInfo.folder, *fileInfo.fileName);
+                break;
+            }
+            case OperationCommand::CREATE_FILE: {
+                add_file(fileInfo.folder, *fileInfo.fileName);
+                break;
+            }
+            case OperationCommand::RENAME_FOLDER: {
+                folder_change_name(fileInfo.folder, *fileInfo.fileName);
+                break;
+            }
+            case OperationCommand::RENAME_FILE: {
+                file_change_name(fileInfo.file, *fileInfo.fileName);
+                break;
+            }
+            default:
+                break;
         }
-
-        // 将数据存入磁盘块
-        DirectoryManager::saveData(dataInfo, directory, fileName);
-
-        // 调用目录管理功能为其在目录中建立目录项
-        DirectoryManager::createDirectoryEntry(directory, fileName, allocatedBlocks);
-
-        // 更改空闲盘块信息
-        DirectoryManager::updateFreeBlocks();
     }
 
     void destroy() override {
@@ -279,6 +313,12 @@ public:
         processManager.deleteProcess(this->pid);
         // 销毁对象
         delete this;
+    }
+    void judge_is_repeat(Folder *folder, std::string str) {
+        if (is_repeat(folder, str)) {
+            //todo 在这里向QT发送重名信息
+            return;
+        }
     }
 };
 
@@ -292,22 +332,30 @@ public:
             : Process(name, pid, priority, state, type) {}
 
     static void
-    create(string name, int pid, int priority, const std::string &directory, const std::string &fileName) {
-        auto* dataDeletionProcess=new DataDeletionProcess(name, pid, priority, ProcessState::READY,
-                                                ProcessType::DATA_DELETION_PROCESS);
-        dataDeletionProcess->fileName = fileName;
-        dataDeletionProcess->directory = directory;
+    create(string name, int pid, int priority, FileInfo* fileInfo,OperationCommand command) {
+        auto *dataDeletionProcess = new DataDeletionProcess(name, pid, priority, ProcessState::READY,
+                                                            ProcessType::DATA_DELETION_PROCESS);
+        dataDeletionProcess->fileInfo.fileName=fileInfo->fileName;
+        dataDeletionProcess->fileInfo.folder=fileInfo->folder;
+        dataDeletionProcess->command=command;
         // 将该进程放入进程列表,就绪队列
         processManager.processList.push_back(dataDeletionProcess);
         processManager.readyQueue.push(dataDeletionProcess);
     }
 
     void execute() override {
-        // 从目录中删除目录项并释放磁盘块,需要注意的是,如果当前数据在内存中,则不能删除
-        DirectoryManager::deleteDirectoryEntry(directory, fileName);
-
-        // 更改空闲盘块信息
-        DirectoryManager::updateFreeBlocks();
+        switch (command) {
+            case OperationCommand::DELETE_FOLDER: {
+                delete_folder(fileInfo.folder);
+                break;
+            }
+            case OperationCommand::DELETE_FILE: {
+                delete_file(fileInfo.file);
+                break;
+            }
+            default:
+                break;
+        }
     }
 
     void destroy() override {
@@ -328,8 +376,6 @@ enum class UserInputCommand {
     READ_DATA = 1,
     // 写入数据
     WRITE_DATA = 2,
-    // 保存数据
-    SAVE = 3,
 };
 
 /**
@@ -341,32 +387,30 @@ public:
     ExecutionProcess(string &name, int pid, int priority, ProcessState state, ProcessType type)
             : Process(name, pid, priority, state, type) {}
 
-    void send_data_to_pipe(const std::string&data , const std::string&pipeName){
+    void send_data_to_pipe(const std::string &data, const std::string &pipeName) {
         NamedPipe pipe(pipeName);
         pipe.writeData(data);
     }
-    std::string receive_data_from_pipe(const std::string&pipeName){
+
+    std::string receive_data_from_pipe(const std::string &pipeName) {
         NamedPipe pipe(pipeName);
         return pipe.readData();
     }
-    static void create(string name, int pid, int priority, const std::string &directory, const std::string &fileName) {
-        auto* executionProcess=new ExecutionProcess(name, pid, priority, ProcessState::READY, ProcessType::EXECUTION_PROCESS);
-        executionProcess->directory = directory;
-        executionProcess->fileName = fileName;
+
+    static void create(string name, int pid, int priority, FileInfo* fileInfo,OperationCommand command) {
+        auto *executionProcess = new ExecutionProcess(name, pid, priority, ProcessState::READY,
+                                                      ProcessType::EXECUTION_PROCESS);
+        executionProcess->fileInfo.file=fileInfo->file;
+        executionProcess->command=command;
+        //todo 申请内存
+
         // 将该进程放入进程列表,就绪队列
         processManager.processList.push_back(executionProcess);
         processManager.readyQueue.push(executionProcess);
     }
 
     void execute() override {
-        std::vector<int> allocatedMemory = DirectoryManager::allocateMemory(8);
-        if (allocatedMemory.empty()) {
-            std::cout << "Error: Not enough free memory for execution." << std::endl;
-            return;
-        }
-        // 根据文件存储信息,将数据从外存读入内存块,如果内存块不够,模拟页面调度,更新页表
-        DirectoryManager::read_data_to_memory(directory, fileName);
-        execute_user_input_command(allocatedMemory);
+        execute_user_input_command(fileInfo.file);
     }
 
     void destroy() override {
@@ -376,25 +420,25 @@ public:
     }
 
     // 模拟执行用户输入的命令,参数为内存块地址
-    static void execute_user_input_command(std::vector<int> &allocatedMemory) {
-        int userInput = 0;
+    static void execute_user_input_command(File* file) {
         while (true) {
-            std::cout << "waiting for user input..." << std::endl;
-            std::cin >> userInput;
-            switch (static_cast<UserInputCommand>(userInput)) {
-                case UserInputCommand::READ_DATA:
-                    // 读取数据,如果该内存块的锁是读锁还是写锁,如果是写锁,则等待
-                    std::cout << "Data read from memory: " << DirectoryManager::read_data(allocatedMemory) << std::endl;
+            //todo 从QT不停获取用户输入的指令，比如读，写
+            UserInputCommand userInputCommand;
+            switch (userInputCommand) {
+                case UserInputCommand::READ_DATA:{
+                    std::string data=look_file_content(file);
+                    //todo 在这里向QT发送data
                     break;
-                case UserInputCommand::WRITE_DATA:
-                    // 写入数据,等待QT界面用户输入,这里模拟一下数据吧,如果该内存块的锁是读锁还是写锁,如果是写锁,则等待
-                    std::cout << "Data written to memory." << std::endl;
-                    DirectoryManager::save_data("this is a test!", allocatedMemory);
+                }
+                case UserInputCommand::WRITE_DATA:{
+                    //todo 从QT获取用户输入的data
+                    std::string data;
+                    file_change_content(file,data);
                     break;
-                case UserInputCommand::SAVE:
-                    // 保存数据,如果该内存块的锁是读锁还是写锁,如果是写锁,则等待
-                    DirectoryManager::write_data_to_disk(allocatedMemory);
-                    break;
+                }
+                case UserInputCommand::EXIT:
+
+                    return;
                 default:
                     break;
             }
@@ -472,68 +516,96 @@ public:
 
 };
 
-// int main() {
-//     // 创建Init进程用于初始化
-//     TaskScheduler taskScheduler;
-//     InitProcess initProcess = InitProcess::create("init", PIDGenerator::generatePID(), 0, ProcessType::INIT_PROCESS);
-//     initProcess.execute();
-//     // 循环等待指令输入
-//     while (true) {
-//         if (!processManager.readyQueue.empty()) {
-//              taskScheduler.createThreads(1);
-//             // taskScheduler.joinThreads();
-//         }
-//         int commandInput = 0;
-//         std::cout << "waiting for command input..." << std::endl;
-//         std::cin >> commandInput;
-//         switch (static_cast<Command>(commandInput)) {
-//             case Command::CREATE_PROCESS:
-//                 int processType;
-//                 std::cout << "Please input process type: " << std::endl;
-//                 std::cin >> processType;
-//                 // 创建进程
-//                 //todo 从QT获取directory和fileName,这里加上直接给出
-//                 switch (static_cast<ProcessType>(processType)) {
-//                     case ProcessType::DATA_GENERATION_PROCESS: {
-//                         std::string directory_input;
-//                         std::string fileName_input;
-//                         int pid = PIDGenerator::generatePID();
-//                         DataGenerationProcess::create(
-//                                 &"dataGenerationProcess"[pid], pid, 1, directory_input, fileName_input);
-//                         break;
-//                     }
-//                     case ProcessType::DATA_DELETION_PROCESS: {
-//                         std::string directory_input;
-//                         std::string fileName_input;
-//                         int pid = PIDGenerator::generatePID();
-//                         DataDeletionProcess::create(
-//                                 &"dataDeletionProcess"[pid], PIDGenerator::generatePID(), 1, directory_input,
-//                                 fileName_input);
-//                         break;
-//                     }
-//                     case ProcessType::EXECUTION_PROCESS: {
-//                         std::string directory_input;
-//                         std::string fileName_input;
-//                         int pid = PIDGenerator::generatePID();
-//                         ExecutionProcess::create(&"executionProcess"[pid], pid, 1,
-//                                                                                      directory_input, fileName_input);
-//                         break;
-//                     }
-//                     default:
-//                         break;
-//                 }
-//                 break;
-//             case Command::EXIT:
-//                 // 销毁进程,释放内存
-//                 //找到所有进程,调用destroy方法,第一个进程是init进程,最后销毁
-//                 for (int i = 1; i < processManager.processList.size(); i++) {
-//                     processManager.processList[i]->destroy();
-//                 }
-//                 processManager.processList[0]->destroy();
-//                 return 0;
-//             default:
-//                 break;
-//         }
-//         // taskScheduler.joinThreads();
-//     }
-// }
+int main() {
+    // 创建Init进程用于初始化
+    TaskScheduler taskScheduler;
+    InitProcess::create("init", PIDGenerator::generatePID(), 0, ProcessType::INIT_PROCESS).execute();
+    // 循环等待指令输入
+    while (true) {
+        if (!processManager.readyQueue.empty()) {
+            taskScheduler.createThreads(1);
+            // taskScheduler.joinThreads();
+        }
+        int commandInput = 0;
+        //todo 在这里从QT获取commandInput
+        std::cout << "waiting for command input..." << std::endl;
+        std::cin >> commandInput;
+        switch (static_cast<OperationCommand>(commandInput)) {
+            case OperationCommand::CREATE_FOLDER: {
+                //todo 从QT获取目录的指针folder和文件名str
+                Folder *folder;
+                std::string file_name;
+                FileInfo fileInfo = {nullptr, folder, &file_name, nullptr};
+                int pid = PIDGenerator::generatePID();
+                DataGenerationProcess::create(
+                        &"dataGenerationProcess"[pid], pid, 1, &fileInfo, OperationCommand::CREATE_FOLDER);
+                break;
+            }
+            case OperationCommand::DELETE_FOLDER: {
+                //todo 从QT获取待删除文件夹的指针
+                Folder *folder;
+                FileInfo fileInfo = {nullptr, folder, nullptr, nullptr};
+                int pid = PIDGenerator::generatePID();
+                DataDeletionProcess::create(
+                        &"dataGenerationProcess"[pid], pid, 1, &fileInfo, OperationCommand::DELETE_FOLDER);
+                break;
+            }
+            case OperationCommand::CREATE_FILE: {
+                //todo 从QT获取目录的指针folder和文件名str
+                Folder *folder;
+                std::string file_name;
+                FileInfo fileInfo = {nullptr, folder, nullptr, nullptr};
+                int pid = PIDGenerator::generatePID();
+                DataGenerationProcess::create(
+                        &"dataGenerationProcess"[pid], pid, 1, &fileInfo, OperationCommand::CREATE_FILE);
+                break;
+            }
+            case OperationCommand::DELETE_FILE: {
+                //todo 从QT获取待删除文件的指针
+                File *file;
+                FileInfo fileInfo = {file, nullptr, nullptr, nullptr};
+                int pid = PIDGenerator::generatePID();
+                DataDeletionProcess::create(
+                        &"dataGenerationProcess"[pid], pid, 1, &fileInfo, OperationCommand::DELETE_FILE);
+                break;
+            }
+            case OperationCommand::CREATE_READ_WRITE:{
+                //todo 从QT获取待读写文件的指针
+                File *file;
+                FileInfo fileInfo = {file, nullptr, nullptr, nullptr};
+                int pid = PIDGenerator::generatePID();
+                ExecutionProcess::create(
+                        &"executionProcess"[pid], pid, 1, &fileInfo, OperationCommand::CREATE_READ_WRITE);
+            }
+            case OperationCommand::RENAME_FOLDER: {
+                //todo 从QT获取待重命名文件夹的指针和新的文件夹名
+                Folder *folder;
+                std::string new_name;
+                FileInfo fileInfo = {nullptr, folder, &new_name, nullptr};
+                int pid = PIDGenerator::generatePID();
+                DataGenerationProcess::create(
+                        &"dataGenerationProcess"[pid], pid, 1, &fileInfo, OperationCommand::RENAME_FOLDER);
+            }
+            case OperationCommand::RENAME_FILE:{
+                //todo 从QT获取待重命名文件的指针和新的文件名
+                File *file;
+                std::string new_name;
+                FileInfo fileInfo = {file, nullptr, &new_name, nullptr};
+                int pid = PIDGenerator::generatePID();
+                DataGenerationProcess::create(
+                        &"dataGenerationProcess"[pid], pid, 1, &fileInfo, OperationCommand::RENAME_FILE);
+            }
+            case OperationCommand::EXIT: {
+                // 销毁进程,释放内存
+                //找到所有进程,调用destroy方法,第一个进程是init进程,最后销毁
+                for (int i = 1; i < processManager.processList.size(); i++) {
+                    processManager.processList[i]->destroy();
+                }
+                processManager.processList[0]->destroy();
+                return 0;
+            }
+
+        }
+        // taskScheduler.joinThreads();
+    }
+}
